ce732665ae9932b2523838e2b88d4653539b7b11
Luis <luisvaleroc@gmail.com>
2024-03-10 14:12:44 -0300

    first commit

---
+++ src/main/java/com/interceptors/interceptors/MvcConfig.java
@@ -0,0 +1,26 @@
+package com.interceptors.interceptors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.rmi.registry.Registry;
+
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    @Qualifier("timeInterceptor")
+    private HandlerInterceptor timeInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+       //registry.addInterceptor(timeInterceptor);
+       registry.addInterceptor(timeInterceptor).addPathPatterns("/app/bar", "/app/foo");
+       //registry.addInterceptor(timeInterceptor).addPathPatterns("/app/bar", "/app/**"); cualquier ruta dentro de app
+        // registry.addInterceptor(timeInterceptor).excludePathPatterns("/app/bar", "/app/foo");
+    }
+}
